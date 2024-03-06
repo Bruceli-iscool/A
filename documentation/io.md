acc7fe03eced6823f4b41d7802870f380cc9eb6c
Bruce Li <isanoob1897@outlook.com>
2024-03-06 01:25:28 +0000

    add

---
+++ documentation/io.md
@@ -0,0 +1,7 @@
+# A's input and output stream
+
+In programming, input and output is very important. In A output can be done with the 'echo' function. For example:
+
+        echo 'hi';
+        echo 1 + 55/5;
+The 'echo' function does not print a newline at the end. Nor does the math libaries .printnf function.
--- src/interpret.py
+++ src/interpret.py
@@ -4,9 +4,41 @@ import paren
 
 def interpret(line):
     # find tokens and execute them
-    if line.startswith("return"):
-        line = line.lstrip('return ')
+    if line.startswith("#"):
+        pass
+    if line.startswith("echo"):
+        line = line.lstrip('echo ')
         if ';' not in line:
             print("a: An error occured: missing a ';'")
-        line = line.rstrip(';')
-interpret('return 5')
+        else:
+            try:
+                line = line.rstrip(';')
+                print(eval(line), end="")
+            except Exception as e:
+                line = line.rstrip(';')
+                print(line, end="")
+    if line.startswith("math."):
+        # math libary
+        line = line.lstrip("math.")
+        if line.startswith("printnf."):
+            line = line.lstrip("printnf.")
+            if line.startswith("sum("):
+                line = line.lstrip("sum(")
+                line = line.rstrip(")")
+                strline = line.replace(" ", "")
+                if '+' in strline and '-' and '*' and '/' and '%' not in strline:
+                    print(eval(strline), end="")
+                else:
+                    print("a: An error occured: Unsupported operation")
+            elif line.startswith("sub("):
+                line = line.lstrip("sum(")
+                line = line.rstrip(")")
+                line = line.replace(" ", "")
+                if '-' in line and '+' and '*' and '/' and '%' not in strline:
+                    print(eval(strline), end="")
+                else:
+                    print("a: An error occured: Unsupported operation")
+    
+
+interpret("echo 'hi'")
+interpret("math.printnf.sum(1 + 1 )")
