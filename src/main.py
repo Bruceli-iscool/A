b2d23836d8bd8a56122d758b594bf143bbf972a5
Bruce Li <isanoob1897@outlook.com>
2024-03-05 20:39:13 -0800

    add

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 import interpret
-
-
+import open_file
+import sys
 
 def main():
     print("The A interpreter.\nlicense() for license or exit() to exit()\nType .test to use the interactive interperter.\n")
     while True:
         userinput = input(">")
+        if userinput.startswith("license()"):
+            print("The A programming language is licensed under the Apache 2.0 license.\nThe license can be found here: https://www.apache.org/licenses/LICENSE-2.0.")
+        elif userinput.startswith("exit()"):
+            sys.exit()
+        elif userinput.startswith(".test"):
+            pass
+        else:
+            try:
+                open_file.open_file(userinput)
+            except Exception as e:
+                print(f"a: An error occured: {e}")
+main()
